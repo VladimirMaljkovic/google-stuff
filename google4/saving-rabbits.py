df7b261a2a8ca1b5c6f1b14f4d8c1daf4d7e4aa7
Vladimir Maljkovic <maljkovic.vladimir@gmail.com>
2020-11-14 14:44:47 +0100

    expanded further on the idea

--- google4/saving-rabbits.py
+++ google4/saving-rabbits.py
@@ -12,5 +12,28 @@ gledam sta ce se desiti
 dodajem kontrolu za infinite loop
 pokusavam da se usmerim (maybe) ka cilju u odnosu na vreme
 plakacu
+
+marko:
+da napravim listu apsolutno svih mogucnosti
+tipa
+kad sam u o,o
+da pisem t i delta t za svako polje na koje mogu da idem
+pa onda pravim novu listu gde upisujem sva ostala polja ged mogu da idem
+to je sve super ali jebeno nemam pojma kako da se krecem ja kroz tu strukturu posle
 """
 
+
+def attempt1(m, time):
+    pass
+
+
+if __name__ == '__main__':
+    matrix = [
+        [0, 2, 2, 2, -1],  # 0 = Start
+        [9, 0, 2, 2, -1],  # 1 = Bunny 0
+        [9, 3, 0, 2, -1],  # 2 = Bunny 1
+        [9, 3, 2, 0, -1],  # 3 = Bunny 2
+        [9, 3, 2, 2, 0],  # 4 = Bulkhead
+    ]
+
+    attempt1(matrix, 1)
