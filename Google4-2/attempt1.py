9df20f28b26c2b08bcc8c3fb9cf1a2928e25cfa6
Vladimir Maljkovic <maljkovic.vladimir@gmail.com>
2020-11-26 17:24:25 +0100

    getting closer

--- Google4-2/attempt1.py
+++ Google4-2/attempt1.py
@@ -1,6 +1,141 @@
-def solution(entrances, exits, path):
+import sys
+from collections import defaultdict
+
+infinite = sys.maxsize
+
+
+def normalize(entrances, exits, path):
+    length = len(path)
+    new_length = length + 2
+    new_path = [[0 for i in range(new_length)] for j in range(new_length)]
+
+    for i in range(length):
+        for j in range(length):
+            new_path[i+1][j+1] = path[i][j]
+
+    # print('filled new_path:')
+    # for i in range(new_length):
+    #     for j in range(new_length):
+    #         print(f'{new_path[i][j]} ', end='')
+    #     print()
+    # print()
+
+    for entrance in entrances:
+        new_path[0][entrance+1] = infinite
+    for eexit in exits:
+        new_path[eexit+1][new_length-1] = infinite
+    print()
+
+    # for i in range(new_length):
+    #     for j in range(new_length):
+    #         print(f'{new_path[i][j]} ', end='')
+    #     print()
+
+    return new_path
+
+
+def solve_using_ford_fulkerson(normalized_network):
     pass
 
 
+# https://en.wikipedia.org/wiki/Ford%E2%80%93Fulkerson_algorithm#Algorithm
+# https://en.wikipedia.org/wiki/Edmonds%E2%80%93Karp_algorithm
+# https://www.geeksforgeeks.org/ford-fulkerson-algorithm-for-maximum-flow-problem/
+# https://en.wikipedia.org/wiki/Breadth-first_search
+
+
+class Graph:
+
+    def __init__(self, graph):
+        self.graph = graph  # residual graph
+        self.ROW = len(graph)
+        # self.COL = len(gr[0])
+
+    '''Returns true if there is a path from source 's' to sink 't' in 
+    residual graph. Also fills parent[] to store the path '''
+
+    def BFS(self, s, t, parent):
+
+        # Mark all the vertices as not visited
+        visited = [False] * self.ROW
+
+        # Create a queue for BFS
+        queue = list()
+
+        # Mark the source node as visited and enqueue it
+        queue.append(s)
+        visited[s] = True
+
+        # Standard BFS Loop
+        while queue:
+
+            # Dequeue a vertex from queue and print it
+            u = queue.pop(0)
+
+            # Get all adjacent vertices of the dequeued vertex u
+            # If a adjacent has not been visited, then mark it
+            # visited and enqueue it
+            for ind, val in enumerate(self.graph[u]):
+                if visited[ind] is False and val > 0:
+                    queue.append(ind)
+                    visited[ind] = True
+                    parent[ind] = u
+
+                    # If we reached sink in BFS starting from source, then return
+        # true, else false
+        return True if visited[t] else False
+
+    # Returns tne maximum flow from s to t in the given graph
+    def FordFulkerson(self, source, sink):
+
+        # This array is filled by BFS and to store path
+        parent = [-1] * self.ROW
+
+        max_flow = 0  # There is no flow initially
+
+        # Augment the flow while there is path from source to sink
+        while self.BFS(source, sink, parent):
+
+            # Find minimum residual capacity of the edges along the
+            # path filled by BFS. Or we can say find the maximum flow
+            # through the path found.
+            path_flow = float("Inf")
+            s = sink
+            while s != source:
+                path_flow = min(path_flow, self.graph[parent[s]][s])
+                s = parent[s]
+
+                # Add path flow to overall flow
+            max_flow += path_flow
+
+            # update residual capacities of the edges and reverse edges
+            # along the path
+            v = sink
+            while v != source:
+                u = parent[v]
+                self.graph[u][v] -= path_flow
+                self.graph[v][u] += path_flow
+                v = parent[v]
+
+        return max_flow
+
+
 if __name__ == '__main__':
-    solution(None, None, None)
+    entrances = [0, 1]
+    exits = [4, 5]
+    path = [
+        [0, 0, 4, 6, 0, 0],  # Room 0: Bunnies
+        [0, 0, 5, 2, 0, 0],  # Room 1: Bunnies
+        [0, 0, 0, 0, 4, 4],  # Room 2: Intermediate room
+        [0, 0, 0, 0, 6, 6],  # Room 3: Intermediate room
+        [0, 0, 0, 0, 0, 0],  # Room 4: Escape pods
+        [0, 0, 0, 0, 0, 0],  # Room 5: Escape pods
+    ]
+
+    # solve_using_ford_fulkerson(normalize(entrances, exits, path))
+
+    g = Graph(normalize(entrances, exits, path))
+    source = 0
+    sink = len(path) + 2
+
+    print(f'Max possible flow is {g.FordFulkerson(source, sink)}')
